0a023c4cc14b9eb5218683d23f0e2533a0f8a894
Luca <devluca15@gmail.com>
2022-07-16 21:02:31 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { RifugioService } from './services/rifugio.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app-esame-finale';
+  
+  public rifugi: any[] = [];
+  public rifugio: any;
+  
+  public modalStatus: boolean = false;
+
+  public myObj = {
+    id: 100,
+    name: 'My new Obj',
+  };
+
+  constructor(public rifugioService: RifugioService) {}
+
+  ngOnInit(): void {
+    this.rifugioService.getAllRifugio().subscribe((data: any) => this.rifugi = data);
+  }
+
+  visualizzaRifugio(rifugio: any) {
+    this.rifugio = rifugio;
+    this.modalStatus = true;
+  }
+
+  closeModal() {
+    this.modalStatus = false;
+  }
+
+  getRifugioById() {
+    this.rifugioService.getRifugioById(1).subscribe((data) => console.log(data));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+import { AccordionModule } from 'primeng/accordion';
+import { TagModule } from 'primeng/tag';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ButtonModule,
+    CardModule,
+    DialogModule,
+    AccordionModule,
+    TagModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
